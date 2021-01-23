de69a3d2891260918ac665b30c4a2367b970020f
cldieder.btc <claas.diederichs@btc-ag.com>
2021-01-23 14:34:09 +0100

    removed test file

--- test.py
+++
@@ -1,5 +0,0 @@
-from openapi_func_get.schema import generate_schemas
-if __name__ == "__main__":
-    generate_schemas("D:/data/btc/buntspecht/UserManagement/apis/service-api/userservice.openapi.yml",
-                     "D:/data/btc/buntspecht/UserManagement/UserManagement/UserManagementFunction/Model",
-                     "UserManagementFunction.Model")

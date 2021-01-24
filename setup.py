427430a244febe4f6997023754b6897189e1f858
cldieder.btc <claas.diederichs@btc-ag.com>
2021-01-24 17:04:21 +0100

    added nullable, default and Required properties

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='caffoa',
-    version='0.3.1',
+    version='0.4.0',
     packages=['caffoa'],
     package_data={
         'caffoa': ['./data/templates/*'],

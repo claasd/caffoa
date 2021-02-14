9964c8b355c7fba774cdbf9d9ea4f90d61d67e67
cldieder.btc <claas.diederichs@btc-ag.com>
2021-02-14 23:33:51 +0100

    fixed invocation attribute

--- caffoa/data/templates/FunctionMethod.cs
+++ caffoa/data/templates/FunctionMethod.cs
@@ -1,7 +1,7 @@
         /// <summary>
         /// auto-generated function invocation.
         ///</summary>
-
+        [FunctionName("{NAME}")]
         public static async Task<HttpResponseMessage> {NAME}(
             [HttpTrigger(AuthorizationLevel.Function, "{OPERATION}", Route = "{PATH}")]
             HttpRequestMessage req{PARAMS}, ILogger log)

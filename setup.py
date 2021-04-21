f8df7ba84f1e3412ca6b279cc46231be8f97a537
cldieder.btc <claas.diederichs@btc-ag.com>
2021-04-21 17:11:47 +0200

    fix same name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='caffoa',
-    version='0.16.0',
+    version='0.16.1',
     packages=['caffoa'],
     package_data={
         'caffoa': ['./data/templates/*'],

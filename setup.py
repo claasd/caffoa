2dc51a8b9569ffd04fa32b9a9d27882a4b120678
Claas Diederichs <claas.diederichs@btc-ag.com>
2021-10-10 15:31:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='caffoa',
-    version='0.30.1',
+    version='0.31.0',
     packages=['caffoa'],
     package_data={
         'caffoa': ['./data/**/*'],
